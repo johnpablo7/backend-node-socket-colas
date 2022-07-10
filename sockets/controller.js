91190bd1b948c164af8c9f96c5288f69e7cf4217
john <john.davis.2015.03@gmail.com>
2022-07-10 14:24:05 -0500

    Clase 220 Mostrar cola de tickets en pantalla

---
+++ sockets/controller.js
@@ -0,0 +1,49 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl = new TicketControl();
+
+const socketController = (socket) => {
+
+  socket.emit("ultimo-ticket", ticketControl.ultimo);
+  socket.emit("estado-actual", ticketControl.ultimos4);
+
+  socket.on("siguiente-ticket", (payload, callback) => {
+
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente);
+
+    // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+  });
+
+  socket.on("atender-ticket", ({ escritorio }, callback) => {
+    // console.log(payload);
+
+    if (!escritorio) {
+      return callback({
+        ok: false,
+        msg: "El escritorio es obligatorio",
+      });
+    }
+
+    const ticket = ticketControl.atenderTicket(escritorio);
+
+    // TODO: notificar cambio en los ultimos4
+    socket.broadcast.emit("estado-actual", ticketControl.ultimos4);
+
+    if (!ticket) {
+      callback({
+        ok: false,
+        msg: "Ya no hay tickets pendientes",
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket,
+      });
+    }
+  });
+};
+
+module.exports = {
+  socketController,
+};
